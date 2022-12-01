efb9ef2947461847810243b752b99af8276ebcfd
Hristo <hsg2001@hotmail.com>
2022-12-01 09:46:17 +0100

    01-12-2022

--- Buscaminas/iniciarBuscaminas.js
+++ Buscaminas/iniciarBuscaminas.js
@@ -1,6 +1,22 @@
+window.addEventListener("load",generarTableroJS);
+
 function generarTableroJS() {
     let tamaño=prompt("Dime el tamaño del tablero");
-    
+
+    for(let i=0; i<tamaño; i++) {
+        let miTabla=document.createElement("table");
+        miTabla.setAttribute("class","tamañoCeldas");
+        let filas=document.createElement("tr");
+        filas.setAttribute("class","colorCeldas");
+        let columnas;
+        for(let i=0; i<tamaño; i++) {    
+            columnas=document.createElement("td");
+        }
+        filas.appendChild(columnas);
+        miTabla.appendChild(filas);
+    }
+    let contenido=document.getElementById("miDiv");
+    contenido.appendChild(miTabla);
 };
 
 function numeroAleatorio() {
